7551e50d4e975628a5bd77b042e8fd9275eb0fe9
Catalina Galan <catalina.galan@hotmail.com>
2023-06-06 16:03:28 -0400

    seeds are working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }]
-#   Character.create(name: "Luke", movie: movies.first)
-puts "Cleaning the users table"
-User.destroy_all
-puts "Cleaning the artist table"
-Artist.destroy_all
+
+
+puts "Cleaning the comments table"
+Comment.destroy_all
+puts "Cleaning the reviews table"
+Review.destroy_all
+puts "Cleaning the attendances table"
+Attendance.destroy_all
 puts "Cleaning the concerts table"
 Concert.destroy_all
+puts "Cleaning the artist table"
+Artist.destroy_all
+puts "Cleaning the users table"
+User.destroy_all
 
 puts "creating user seeds Bowie and Paloma"
 User.create!(username: "Bowie", age: 23, city: "Montreal", bio: "Hello there! I am Bowie, a super cool dude from Montreal.",
@@ -30,13 +31,14 @@ Concert.create!(city: "Montreal", venue: "Olympic Stadium", date: DateTime.new(2
 puts "done!"
 
 puts "creating seed attendace"
-Attendance.create!(user_id: User.first, concert_id: Concert.first)
+Attendance.create!(user: User.first, concert: Concert.first)
 puts "done!"
 
 puts "seed writing a review"
-Review.create!(rating: 5, content: "What an incredible show! I blown away. If you get the chance, this is a MUST SEE.",
-               attendance_id: Attendance.first)
+Review.create!(rating: 5, content: "What an incredible show! I'm blown away. If you get the chance, this is a MUST SEE.",
+               attendance: Attendance.first)
 puts "done!"
 
 puts "seed commenting on review"
-Comment.create!(content: "I agree!!!! We are lucky we got to see that!", user_id: User.last, review_id: Review.first )
+Comment.create!(content: "I agree!!!! We are lucky we got to see that!", user: User.last, review: Review.first )
+puts "all done!"
