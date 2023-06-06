a352f1b1489b783897d1a9ee86b71e8090537829
Catalina Galan <catalina.galan@hotmail.com>
2023-06-06 11:58:15 -0400

    working on associations and seed for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create([username: "Bowie", age: 23, city: "Montreal", bio: "Hello there! I am Bowie, a super cool dude from Montreal", email: "bowie@bowie.com", password: "12345")
