034c4cad052c1d3c67eadd54f086ae4b0ca26143
Kirstin Penzes <kpenzes17@gmail.com>
2023-06-06 14:26:03 -0400

    Nothing to add to homepage controller since Review model isn't created yet, and nothing to add to view of homepage since it will be made up of components not yet done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    # will need to provide instances of reviews from users
+    # that current_user follows for homepage
   end
 end
