1e717c814807a2b9687e51e1f595c7fb1ced3c1e
Antoine Marchand <marchand41@gmail.com>
2023-06-06 14:09:13 -0400

    added seeds for concert

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,7 @@ puts "creating user seed Bowie"
 User.create!(username: "Bowie", age: 23, city: "Montreal", bio: "Hello there! I am Bowie, a super cool dude from Montreal",
             email: "bowie@bowie.com", password: "123456")
 puts "done!"
+
+puts "creating a concert seed Led Zeppelin"
+Concert.create!(city: "Montreal", venue: "Olympic Stadium", date: "October 1st, 2022", average_rating: "9.5", artist: "Bowie's favorite band")
+puts "done!"
