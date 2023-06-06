d65db8a984ac6e56eca0c10a8c40015726179f05
Antoine Marchand <marchand41@gmail.com>
2023-06-06 14:22:33 -0400

    added artist and concert seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }]
 #   Character.create(name: "Luke", movie: movies.first)
 
+
 puts "creating user seed Bowie"
-User.create!(username: "Bowie", age: 23, city: "Montreal", bio: "Hello there! I am Bowie, a super cool dude from Montreal",
-            email: "bowie@bowie.com", password: "123456")
+User.create!(username: "Bowie", age: 23, city: "Montreal", bio: "Hello there! I am Bowie, a super cool dude from Montreal", email: "bowie@bowie.com", password: "123456")
 puts "done!"
 
-puts "creating a concert seed Led Zeppelin"
-Concert.create!(city: "Montreal", venue: "Olympic Stadium", date: "October 1st, 2022", average_rating: "9.5", artist: "Bowie's favorite band")
+
+puts "creating artist seed Led Zeppelin "
+Artist.create!(name: "Led Zeppelin")
+puts "done!"
+
+puts "creating a concert seed Olympic Stadium - Led Zeppelin"
+Concert.create!(city: "Montreal", venue: "Olympic Stadium", date: "October 1st, 2022", average_rating: "9.5", artist: Artist.first)
 puts "done!"
