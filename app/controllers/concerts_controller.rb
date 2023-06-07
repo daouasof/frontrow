c2d089fa154f64170fe00d7fe8c35c19a17197da
Antoine Marchand <marchand41@gmail.com>
2023-06-07 14:59:07 -0400

    adjusted controllers

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -3,16 +3,11 @@ require 'nokogiri'
 
 class ConcertsController < ApplicationController
   def index
-<<<<<<< HEAD
     @concerts = Concert.all
   end
 
   def show
     @concert = Concert.find(params[:id])
     @review = Review.new
-=======
-    #it wont be concert.all - it will be based on whatever the user has search
-    @concerts = Concert.all
->>>>>>> master
   end
 end
