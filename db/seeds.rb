eb225a0a2f67d2af7d3e5a638009cfdbe0713954
Kirstin Penzes <kpenzes17@gmail.com>
2023-06-07 11:47:13 -0400

    Updated seeds to include destroying db at the beginning, and added API info for one artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }]
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Clearing database..."
+Concert.destroy_all
+puts "Concerts destroyed"
+Artist.destroy_all
+puts "Artists destroyed"
+User.destroy_all
+puts "Users destroyed"
+puts "All done!"
+
 require "json"
 require "rest-client"
 
-url =
-response = RestClient.get(url, { 'Accept': 'application/json', 'x-api-key': SETLIST_API_KEY })
+menzingers = "3071d829-b9ca-4499-b4f5-74d6d8531aed"
+artist = Artist.create(name: "The Menzingers")
+puts "Creating concerts for #{artist.name}"
+
+url = "https://api.setlist.fm/rest/1.0/artist/#{menzingers}/setlists"
+response = RestClient.get(url, { 'Accept': 'application/json', 'x-api-key': ENV['SETLIST_API_KEY'] })
+
+shows = JSON.parse(response)
+# pp shows
+# pp shows.keys
+shows["setlist"].each do |show|
+  concert = Concert.new
+  concert.city = show["venue"]["city"]
+  concert.venue = show["venue"]["name"]
+  concert.date = show["eventDate"]
+  # concert.artist_id = Artist.first
+  concert.save!
+  puts "Created new concert with id #{concert.id}"
+end
+
 
 puts "creating user seed Bowie"
 
