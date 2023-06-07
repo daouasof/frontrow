625bc42c0fd23fc86929548fa3aed6a00b3dfebd
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-06-07 15:16:15 -0400

    installed pgsearch, implemented the first step with the city

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -5,6 +5,9 @@ class ConcertsController < ApplicationController
   def index
     #it wont be concert.all - it will be based on whatever the user has search
     @concerts = Concert.all
+    if params[:query].present?
+      @concerts = Concert.search_by_city(params[:query])
+    end
   end
 end
 
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,4 +1,11 @@
 class Concert < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_city,
+    against: [ :city],
+    using: {
+      tsearch: { prefix: true }
+    }
+
   belongs_to :artist
   has_many :attendances
   has_many :users, through: :attendances
