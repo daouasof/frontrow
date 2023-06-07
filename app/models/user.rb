abbec9d9a6d33b38d0e0a47e5c78ec7973642294
Antoine Marchand <marchand41@gmail.com>
2023-06-07 14:51:29 -0400

    Improvement to user model regarding attendances

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User < ApplicationRecord
   has_many :attendances
   has_many :reviews, through: :attendances
   has_many :concerts, through: :attendances
+
+  def attended?(concert)
+    !find_attendance(concert).nil?
+  end
+
+  def attendance_info(concert)
+    find_attendance(concert)
+  end
+
+  private
+
+  def find_attendance(concert)
+    Attendance.find_by(concert: concert, user: self)
+  end
 end
