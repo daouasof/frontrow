60acf48072a40858e3139115e7cca775d66a3a0e
Catalina Galan <catalina.galan@hotmail.com>
2023-06-08 16:54:19 -0400

    seeds for bowies review photos working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require "open-uri"
 require "json"
 require "rest-client"
+require_relative "users"
 
 puts "Cleaning the comments table"
 Comment.destroy_all
@@ -15,68 +16,65 @@ Artist.destroy_all
 puts "Cleaning the users table."
 User.destroy_all
 
-# bands = [{ name: 'The Menzingers', url: '3071d829-b9ca-4499-b4f5-74d6d8531aed' },
-#            { name: 'The Gaslight Anthem', url: 'f208f09e-b5b3-4b06-87cd-f7230fae17e3' },
-#            { name: 'Muse', url: '9c9f1380-2516-4fc9-a3e6-f9f61941d090' },
-#            { name: 'Burna Boy', url: '78a19169-ac75-4868-b504-7e2e073118e0' },
-#            { name: 'Polo & Pan', url: '1d9ec7ea-0fa4-41d9-917b-723c735ebbfe' },
-#            { name: 'Louise Attaque', url: '04d25080-32e2-49a2-b638-c9ca4f3e12bd' },
-#            { name: 'Gojira', url: '1c5efd53-d6b6-4d63-9d22-a15025cf5f07' }]
+bands = [{ name: 'The Menzingers', url: '3071d829-b9ca-4499-b4f5-74d6d8531aed' },
+           { name: 'The Gaslight Anthem', url: 'f208f09e-b5b3-4b06-87cd-f7230fae17e3' },
+           { name: 'Muse', url: '9c9f1380-2516-4fc9-a3e6-f9f61941d090' },
+           { name: 'Burna Boy', url: '78a19169-ac75-4868-b504-7e2e073118e0' },
+           { name: 'Polo & Pan', url: '1d9ec7ea-0fa4-41d9-917b-723c735ebbfe' },
+           { name: 'Louise Attaque', url: '04d25080-32e2-49a2-b638-c9ca4f3e12bd' },
+           { name: 'Gojira', url: '1c5efd53-d6b6-4d63-9d22-a15025cf5f07' }]
 
-# bands.each do |band|
-#   sleep(3)
-#   puts "Creating the artist #{band[:name]}"
-#   artist = Artist.create(name: "#{band[:name]}")
-#   puts "Creating concerts for #{band[:name]}"
+bands.each do |band|
+  sleep(3)
+  puts "Creating the artist #{band[:name]}"
+  artist = Artist.create(name: "#{band[:name]}")
+  puts "Creating concerts for #{band[:name]}"
 
-#   url = "https://api.setlist.fm/rest/1.0/artist/#{band[:url]}/setlists"
+  url = "https://api.setlist.fm/rest/1.0/artist/#{band[:url]}/setlists"
 
-#   response = RestClient.get(url, { 'Accept': 'application/json', 'x-api-key': ENV['SETLIST_API_KEY'] })
-#   shows = JSON.parse(response)
+  response = RestClient.get(url, { 'Accept': 'application/json', 'x-api-key': ENV['SETLIST_API_KEY'] })
+  shows = JSON.parse(response)
 
-#     shows["setlist"].each do |show|
-#       concert = Concert.new
-#       concert.city = show["venue"]["city"]["name"]
-#       concert.venue = show["venue"]["name"]
-#       concert.date = show["eventDate"]
-#       concert.artist_id = artist.id
-#       concert.save!
-#       puts "Created new concert with id #{concert.id} for #{band[:name]}"
-#     end
-# end
+    shows["setlist"].each do |show|
+      concert = Concert.new
+      concert.city = show["venue"]["city"]["name"]
+      concert.venue = show["venue"]["name"]
+      concert.date = show["eventDate"]
+      concert.artist_id = artist.id
+      concert.save!
+      puts "Created new concert with id #{concert.id} for #{band[:name]}"
+    end
+end
 
-banner = URI.open()
-avatar = URI.open()
 
 puts "creating users seeds Bowie, Paloma and us!"
 
+default_avatar_url = "https://hips.hearstapps.com/hmg-prod/images/sigourney-weaver-avatar-ii-the-way-of-water-1670323174.jpg?crop=0.500xw:0.949xh;0.299xw,0.0514xh&resize=1200:*"
+default_banner_url = "https://townsquare.media/site/62/files/2021/11/attachment-brian-ruiz.jpg?w=980&q=75"
+default_password = "123456"
+default_city = "Montreal"
 
 
-bowie = User.new(username: "Bowie", age: 23, city: "Montreal", bio: "Hello there! I am Bowie, a super cool dude from Montreal.",
-             email: "bowie@bowie.com", password: "123456")
-bowie.avatar.attach(io: avatar, filename: "avatar.png", content_type: "image/png")
-bowie.banner.attach(io: banner, filename: "banner.png", content_type: "image/png")
-bowie.save!
+USERS.each do |user_hash|
+  p user_hash
+  new_user = User.new(
+    username: user_hash[:username],
+    age: user_hash[:age],
+    bio: user_hash[:bio],
+    email: user_hash[:email]
+  )
 
-User.create!(username: "Paloma", age: 24, city: "Montreal", bio: "Hi everyone! Paloma here, happy to go to shows anytime
-             I can.", email: "paloma@paloma.com", password: "123456")
-puts "done!"
+  avatar = URI.open(user_hash[:avatar_url] || default_avatar_url)
+  new_user.avatar.attach(io: avatar, filename: "avatar.png", content_type: "image/png")
+  banner = URI.open(user_hash[:banner_url] || default_banner_url)
+  new_user.banner.attach(io: banner, filename: "banner.png", content_type: "image/png")
 
-User.create!(username: "Cat", age: 42, city: "Montreal", bio: "Hey guys! Cat here, avid rock/metal concert goer ;)",
-             email: "cat@frontrow.com", password: "123456")
-puts "done!"
+  new_user.city = default_city unless user_hash[:city]
+  new_user.password = default_password unless user_hash[:password]
+  new_user.save!
+end
 
-User.create!(username: "Antoine", age: 30, city: "Montreal", bio: " ... bio ...",
-             email: "antoine@frontrow.com", password: "123456")
-puts "done!"
-
-User.create!(username: "Kirstin", age: 29, city: "Montreal", bio: " ... bio ...",
-             email: "kirstin@frontrow.com", password: "123456")
-puts "done!"
-
-User.create!(username: "Sofia", age: 32, city: "Montreal", bio: " ... bio ...",
-             email: "sofia@frontrow.com", password: "123456")
-puts "done!"
+puts "Done!!!"
 
 
 puts "creating seed attendace"
@@ -84,8 +82,19 @@ Attendance.create!(user: User.first, concert: Concert.first)
 puts "done!"
 
 puts "seed writing a review"
-Review.create!(rating: 5, content: "What an incredible show! I'm blown away. If you get the chance, this is a MUST SEE",
-               attendance: Attendance.first, images_url: "https://res.cloudinary.com/dblbiefe6/image/upload/v1686243567/bowie.review.1.jpg")
+bowies_review = Review.new(
+  rating: 5,
+  content: "What an incredible show! I'm blown away. If you get the chance, this is a MUST SEE",
+  attendance: Attendance.first
+)
+bowies_review.photos.attach(io: URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Muse_in_Sydney.jpg/800px-Muse_in_Sydney.jpg"),
+                            filename: "conert_photo_1", content_type: "image/png")
+bowies_review.photos.attach(io: URI.open("https://en.parisinfo.com/var/otcp/sites/images/node_43/node_51/node_77884/node_77888/yoyo-palais-de-tokyo-concert-et-lasers-bleus-%7C-630x405-%7C-%C2%A9-cedric-canezza/11967098-1-fre-FR/Yoyo-Palais-de-Tokyo-Concert-et-lasers-bleus-%7C-630x405-%7C-%C2%A9-Cedric-Canezza.jpg"),
+                            filename: "conert_photo_2", content_type: "image/png")
+bowies_review.photos.attach(io: URI.open("https://turntable.kagiso.io/images/iStock-1181169462.width-800.jpg"),
+                            filename: "conert_photo_3", content_type: "image/png")
+
+bowies_review.save!
 puts "done!"
 
 puts "seed commenting on review"
--- db/users.rb
+++ db/users.rb
@@ -1,38 +1,42 @@
-users = [
+USERS = [
   {
-    username: "Bowie"
-    age: 23
-    bio: "Hello there! I am Bowie, a super cool dude from Montreal."
+    username: "Bowie",
+    age: 23,
+    bio: "Hello there! I am Bowie, a super cool dude from Montreal.",
     email: "bowie@bowie.com"
   },
   {
-    username: "Paloma"
-    age: 24
-    bio: "Hi everyone! Paloma here, happy to go to shows anytime I can."
+    username: "Paloma",
+    age: 24,
+    bio: "Hi everyone! Paloma here, happy to go to shows anytime I can.",
     email: "paloma@paloma.com"
   },
   {
-    username: "Cat"
-    age: 42
-    bio: "Hey guys! Cat here, avid rock/metal concert goer ;)"
-    email: "cat@frontrow.com"
+    username: "Cat",
+    age: 42,
+    bio: "Hey guys! Cat here, avid rock/metal concert goer ;)",
+    email: "cat@frontrow.com",
+    avatar_url: "https://media.istockphoto.com/id/1361956153/photo/black-cat-sticking-out-tongue-funny-portrait.jpg?s=612x612&w=0&k=20&c=JtXn7BNTWl573nyjOJFsI99fXEjctnXsMz5zronzs5A="
   },
   {
-    username: "Antoine"
-    age: 30
-    bio: ""
-    email: "antoine@frontrow.com"
+    username: "Antoine",
+    age: 30,
+    bio: "",
+    email: "antoine@frontrow.com",
+    avatar_url: "https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP2538-CUSA05620_00-AV00000000000036/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=720&h=720"
   },
   {
-    username: "Kirstin"
-    age: 29
-    bio: ""
-    email: "kirstin@frontrow.com"
+    username: "Kirstin",
+    age: 29,
+    bio: "",
+    email: "kirstin@frontrow.com",
+    avatar_url: "https://cdn.dribbble.com/users/473764/screenshots/16521351/media/546fd6236584ca8fb2600cf40ea77518.png?compress=1&resize=400x300&vertical=center"
   },
   {
-    username: "Sofia"
-    age: 32
-    bio: "Tyranny!"
-    email: "sofia@frontrow.com"
-  },
+    username: "Sofia",
+    age: 32,
+    bio: "Tyranny!",
+    email: "sofia@frontrow.com",
+    avatar_url: "https://icons.iconarchive.com/icons/iconarchive/incognito-animals/256/Dog-Avatar-icon.png"
+  }
 ]
