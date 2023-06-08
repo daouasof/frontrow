f608467ef70469f2f47d521a2c14c83e7c885d90
Catalina Galan <catalina.galan@hotmail.com>
2023-06-08 16:05:43 -0400

    reworking seeds, user file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 require "json"
 require "rest-client"
 
@@ -14,47 +15,55 @@ Artist.destroy_all
 puts "Cleaning the users table."
 User.destroy_all
 
-bands = [{ name: 'The Menzingers', url: '3071d829-b9ca-4499-b4f5-74d6d8531aed' },
-           { name: 'The Gaslight Anthem', url: 'f208f09e-b5b3-4b06-87cd-f7230fae17e3' },
-           { name: 'Muse', url: '9c9f1380-2516-4fc9-a3e6-f9f61941d090' },
-           { name: 'Burna Boy', url: '78a19169-ac75-4868-b504-7e2e073118e0' },
-           { name: 'Polo & Pan', url: '1d9ec7ea-0fa4-41d9-917b-723c735ebbfe' },
-           { name: 'Louise Attaque', url: '04d25080-32e2-49a2-b638-c9ca4f3e12bd' },
-           { name: 'Gojira', url: '1c5efd53-d6b6-4d63-9d22-a15025cf5f07' }]
+# bands = [{ name: 'The Menzingers', url: '3071d829-b9ca-4499-b4f5-74d6d8531aed' },
+#            { name: 'The Gaslight Anthem', url: 'f208f09e-b5b3-4b06-87cd-f7230fae17e3' },
+#            { name: 'Muse', url: '9c9f1380-2516-4fc9-a3e6-f9f61941d090' },
+#            { name: 'Burna Boy', url: '78a19169-ac75-4868-b504-7e2e073118e0' },
+#            { name: 'Polo & Pan', url: '1d9ec7ea-0fa4-41d9-917b-723c735ebbfe' },
+#            { name: 'Louise Attaque', url: '04d25080-32e2-49a2-b638-c9ca4f3e12bd' },
+#            { name: 'Gojira', url: '1c5efd53-d6b6-4d63-9d22-a15025cf5f07' }]
 
-bands.each do |band|
-  sleep(3)
-  puts "Creating the artist #{band[:name]}"
-  artist = Artist.create(name: "#{band[:name]}")
-  puts "Creating concerts for #{band[:name]}"
+# bands.each do |band|
+#   sleep(3)
+#   puts "Creating the artist #{band[:name]}"
+#   artist = Artist.create(name: "#{band[:name]}")
+#   puts "Creating concerts for #{band[:name]}"
 
-  url = "https://api.setlist.fm/rest/1.0/artist/#{band[:url]}/setlists"
+#   url = "https://api.setlist.fm/rest/1.0/artist/#{band[:url]}/setlists"
 
-  response = RestClient.get(url, { 'Accept': 'application/json', 'x-api-key': ENV['SETLIST_API_KEY'] })
-  shows = JSON.parse(response)
+#   response = RestClient.get(url, { 'Accept': 'application/json', 'x-api-key': ENV['SETLIST_API_KEY'] })
+#   shows = JSON.parse(response)
 
-    shows["setlist"].each do |show|
-      concert = Concert.new
-      concert.city = show["venue"]["city"]["name"]
-      concert.venue = show["venue"]["name"]
-      concert.date = show["eventDate"]
-      concert.artist_id = artist.id
-      concert.save!
-      puts "Created new concert with id #{concert.id} for #{band[:name]}"
-    end
-end
+#     shows["setlist"].each do |show|
+#       concert = Concert.new
+#       concert.city = show["venue"]["city"]["name"]
+#       concert.venue = show["venue"]["name"]
+#       concert.date = show["eventDate"]
+#       concert.artist_id = artist.id
+#       concert.save!
+#       puts "Created new concert with id #{concert.id} for #{band[:name]}"
+#     end
+# end
+
+banner = URI.open()
+avatar = URI.open()
 
 puts "creating users seeds Bowie, Paloma and us!"
-User.create!(username: "Bowie", age: 23, city: "Montreal", bio: "Hello there! I am Bowie, a super cool dude from Montreal.",
+
+
+
+bowie = User.new(username: "Bowie", age: 23, city: "Montreal", bio: "Hello there! I am Bowie, a super cool dude from Montreal.",
              email: "bowie@bowie.com", password: "123456")
+bowie.avatar.attach(io: avatar, filename: "avatar.png", content_type: "image/png")
+bowie.banner.attach(io: banner, filename: "banner.png", content_type: "image/png")
+bowie.save!
 
 User.create!(username: "Paloma", age: 24, city: "Montreal", bio: "Hi everyone! Paloma here, happy to go to shows anytime
              I can.", email: "paloma@paloma.com", password: "123456")
 puts "done!"
 
 User.create!(username: "Cat", age: 42, city: "Montreal", bio: "Hey guys! Cat here, avid rock/metal concert goer ;)",
-             email: "cat@frontrow.com", password: "123456", avatar_url: "https://res.cloudinary.com/dblbiefe6/image/upload/v1686242897/cat.avatar.jpg",
-             banner_url: "https://res.cloudinary.com/dblbiefe6/image/upload/v1686242903/cat.banner.jpg")
+             email: "cat@frontrow.com", password: "123456")
 puts "done!"
 
 User.create!(username: "Antoine", age: 30, city: "Montreal", bio: " ... bio ...",
---
+++ db/users.rb
@@ -0,0 +1,38 @@
+users = [
+  {
+    username: "Bowie"
+    age: 23
+    bio: "Hello there! I am Bowie, a super cool dude from Montreal."
+    email: "bowie@bowie.com"
+  },
+  {
+    username: "Paloma"
+    age: 24
+    bio: "Hi everyone! Paloma here, happy to go to shows anytime I can."
+    email: "paloma@paloma.com"
+  },
+  {
+    username: "Cat"
+    age: 42
+    bio: "Hey guys! Cat here, avid rock/metal concert goer ;)"
+    email: "cat@frontrow.com"
+  },
+  {
+    username: "Antoine"
+    age: 30
+    bio: ""
+    email: "antoine@frontrow.com"
+  },
+  {
+    username: "Kirstin"
+    age: 29
+    bio: ""
+    email: "kirstin@frontrow.com"
+  },
+  {
+    username: "Sofia"
+    age: 32
+    bio: "Tyranny!"
+    email: "sofia@frontrow.com"
+  },
+]
