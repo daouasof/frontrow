09d26239241bdc50f3a09c7e3d3bf62e523563f9
Antoine Marchand <marchand41@gmail.com>
2023-06-08 15:53:08 -0400

    initial user story complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
   get 'reviews/create'
   devise_for :users
   root to: "pages#home"
+
+  resources :users, only: [:show]
 
   resources :concerts, only: [:index, :show] do
     resources :attendances, only: [:create]
