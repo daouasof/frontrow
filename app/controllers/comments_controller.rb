75e6546283151ed83ebd8ddabf22740503ec9037
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-06-12 16:16:33 -0400

    able to display comments under a review

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,16 @@
 class CommentsController < ApplicationController
 
   def create
+    @comment = Comment.new(set_comment)
+    @comment.user = current_user
+    @comment.review = Review.find(params[:review_id])
+    @comment.save
+  end
 
+  private
+
+  def set_comment
+    params.require(:comment).permit(:content)
   end
 
 end
