3f1b4a1266d57dbbeb2fdc14254ded46bc89c388
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-06-12 15:09:38 -0400

    able to get the id of the review that was clicked

--- app/javascript/controllers/review_comment_controller.js
+++ app/javascript/controllers/review_comment_controller.js
@@ -2,7 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="review-comment"
 export default class extends Controller {
+
+  static values = { reviewId: Number};
+
   connect() {
     console.log("connected");
+  }
+
+  display() {
+    console.log("clicked");
+    console.log(this.reviewIdValue);
   }
 }
