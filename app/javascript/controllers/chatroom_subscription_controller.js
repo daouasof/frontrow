a0cc5bc9ffb4622564ff83876f90a5638c299c77
Catalina Galan <catalina.galan@hotmail.com>
2023-06-13 17:36:43 -0400

    trying to style in broadcasting but getting errors

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number, currentUserId: Number }
   static targets = ["messages"]
 
   connect() {
@@ -14,8 +14,28 @@ export default class extends Controller {
   }
 
   #insertMessageAndScroll(message) {
+    const currentUserIsSender = this.currentUserIdValue === this.data.sender_id
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
     this.messagesTarget.insertAdjacentHTML("beforeend", message)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+    <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+      <div class="${this.#userStyleClass(currentUserIsSender)}">
+        ${message}
+      </div>
+    </div>
+  `
+  }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
   }
 
   resetForm(event) {
