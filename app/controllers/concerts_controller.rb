c09891b45bb11c1f2bd5c6e07d8db9c4b28d1a74
Kirstin Penzes <kpenzes17@gmail.com>
2023-06-13 11:27:12 -0400

    toggle for upcoming shows

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -6,7 +6,6 @@ class ConcertsController < ApplicationController
   def index
     @city = params[:query_city]
     @artist = params[:query_artist]
-    # @concerts = Concert.where('date < ?', Date.today)
     @concerts = Concert.all.order(date: :desc)
     @concerts = Concert.search_by_city(@city) if @city.present?
     @concerts = Concert.search_by_artist(@artist) if @artist.present?
