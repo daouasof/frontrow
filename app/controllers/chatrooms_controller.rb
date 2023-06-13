d693ea1605e000f29e622164aa4387848b152d15
Catalina Galan <catalina.galan@hotmail.com>
2023-06-13 11:13:52 -0400

    chatroom broadcasting and working on stimulus now

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,5 @@
 class ChatroomsController < ApplicationController
   def show
-    # raise
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,7 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
 
-// Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
+  static values = { chatroomId: Number }
+  static targets = ["messages"]
+
   connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: message => this.#insertMessageAndScroll(message) }
+    )
+    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+
+  #insertMessageAndScroll(message) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", message)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 }
