fd6e50f14255d848d9ac2bdf4883f91d39662e79
Kirstin Penzes <kpenzes17@gmail.com>
2023-06-13 16:48:04 -0400

    clearing git status

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2023_06_13_161329) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_203340) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
