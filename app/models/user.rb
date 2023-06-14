07ba6a32b7ad72022d29ea6a25cdbae4e7e5dbdb
Kirstin Penzes <kpenzes17@gmail.com>
2023-06-14 09:31:15 -0400

    moved method to private

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,8 @@ class User < ApplicationRecord
     find_attendance(concert)
   end
 
+  private
+
   def set_default_avatar
     default_avatar_url = "https://hips.hearstapps.com/hmg-prod/images/sigourney-weaver-avatar-ii-the-way-of-water-1670323174.jpg?crop=0.500xw:0.949xh;0.299xw,0.0514xh&resize=1200:*"
     default_banner_url = "https://townsquare.media/site/62/files/2021/11/attachment-brian-ruiz.jpg?w=980&q=75"
@@ -34,8 +36,6 @@ class User < ApplicationRecord
     banner = URI.open(default_banner_url)
     self.banner.attach(io: banner, filename: "banner.png", content_type: "image/png")
   end
-
-  private
 
   def find_attendance(concert)
     Attendance.find_by(concert: concert, user: self)
