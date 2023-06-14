c43d5ba6c69791949280d7be1165373eb19e4107
Catalina Galan <catalina.galan@hotmail.com>
2023-06-14 11:55:54 -0400

    filtering chatrooms for each users index

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,4 +3,24 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
+
+  def index
+    @chatrooms = Chatroom.all
+    @chatrooms_index = []
+    @chatrooms.each do |chat|
+      if current_user.id == chat.participant1_id || current_user.id == chat.participant2_id
+        @chatrooms_index << chat
+      end
+      if current_user.id == chat.participant1_id
+        @other_user = User.find(chat.participant2_id)
+      else
+        @other_user = User.find(chat.participant1_id)
+      end
+    end
+  end
+
+  # def set_chat_user
+  #   # @chat_current_user = current_user
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   resources :attendances, only: :destroy
 
-  resources :chatrooms, only: :show do
+  resources :chatrooms, only: [:show, :index] do
     resources :messages, only: :create
   end
 end
