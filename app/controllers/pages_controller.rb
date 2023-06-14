f4a1680bbd26eb992be4d4d0eb687c59c9cb03d6
Kirstin Penzes <kpenzes17@gmail.com>
2023-06-14 11:49:26 -0400

    Added logic to render concerts of upcoming show near user if user city is present

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    # will need to provide instances of reviews from users
-    # that current_user follows for homepage
     if current_user.present?
       @reviews = Review.all.reject { |review| review.user == current_user }
+      if current_user.city.present?
+        @concerts = Concert.where( { city: current_user.city } )
+      end
     else
       @reviews = Review.all
+      # @concerts = Concert.where( { city: "Montreal"})
     end
   end
 end
