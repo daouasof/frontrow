e187967a9f79091e9d5e0b3a699e15ce38913fde
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-06-14 10:53:05 -0400

    added countValue and  likedValue

--- app/javascript/controllers/review_like_controller.js
+++ app/javascript/controllers/review_like_controller.js
@@ -2,13 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="review-like"
 export default class extends Controller {
+  static targets = ["likes"];
+  static values = {count: Number, liked: Boolean};
+
   connect() {
-    console.log("connected");
+    // console.log("connected");
+    // console.log(this.likesTarget);
+    // console.log(this.likedValue);
   }
 
   update(event) {
-    event.preventDefault();
+    // event.preventDefault();
     console.log("clicked");
+    console.log(this.countValue);
   }
 
 }
