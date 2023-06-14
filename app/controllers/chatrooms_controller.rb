0671cb207666575c549aea0a9a803947e682e2cf
Catalina Galan <catalina.galan@hotmail.com>
2023-06-14 15:29:15 -0400

    chats are ordered by last created

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -5,22 +5,24 @@ class ChatroomsController < ApplicationController
   end
 
   def index
-    @chatrooms = Chatroom.all
-    @chatrooms_index = []
-    @chatrooms.each do |chat|
-      # if current_user.id == chat.participant1_id
-      #   @other_user = User.find(chat.participant2_id)
-      # else
-      #   @other_user = User.find(chat.participant1_id)
-      # end
-      if current_user.id == chat.participant1_id || current_user.id == chat.participant2_id
-        @chatrooms_index << chat
-      end
-    end
+    @chatrooms = Chatroom.get_chatrooms(current_user)
+    @messages = @chatrooms.map { |chat| chat.messages.last }.sort_by(&:created_at).reverse
   end
 
-  # def set_chat_user
-  #   # @chat_current_user = current_user
+  # def unread
+  #   last_message = Chatroom.messages.last_message
+  #   if last_user_id.user_id == current_user.id
+  #     @unread << last_message
+  #   end
   # end
 
+  # def read_or_unread
+  #   @chatroom_index.each do |last_message|
+  #     if last_message.messages.last.user_id == current_user.id
+  #       @unread << last_message
+  #     else
+  #       @read << last_message
+  #     end
+  #   end
+  # end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,12 @@ class Message < ApplicationRecord
   def sender?(a_user)
     user.id == a_user.id
   end
+
+  def find_other_user(a_user)
+    if chatroom.participant1_id == a_user.id
+      User.find(chatroom.participant2_id)
+    else
+      User.find(chatroom.participant1_id)
+    end
+  end
 end
