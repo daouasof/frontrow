152200064fae9834b8906720007c0aaf7749d965
Catalina Galan <catalina.galan@hotmail.com>
2023-06-16 12:03:00 -0400

    Kirstin Vacouver done

--- db/seeds/api_seeds.rb
+++ db/seeds/api_seeds.rb
@@ -219,7 +219,7 @@ USERS.each do |user_hash|
   banner = URI.open(user_hash[:banner_url] || default_banner_url)
   new_user.banner.attach(io: banner, filename: "banner.png", content_type: "image/png")
 
-  new_user.city = default_city unless user_hash[:city]
+  new_user.city = user_hash[:city] || default_city
   new_user.password = default_password unless user_hash[:password]
   new_user.save!
 end
