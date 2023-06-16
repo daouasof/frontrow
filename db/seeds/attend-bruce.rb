7b9e43d0ab7601f70d207a305a91275bf6d0ac33
Catalina Galan <catalina.galan@hotmail.com>
2023-06-16 10:34:30 -0400

    iteration for bruce attendances done

--- db/seeds/attend-bruce.rb
+++ db/seeds/attend-bruce.rb
@@ -6,42 +6,9 @@ cat = User.find_by(username: "Cat")
 Attendance.create!(user: cat, concert: bruce_concert)
 puts "done!"
 
-puts "creating seed Benoit attendance"
-benoit = User.find_by(username: "Benoit")
-Attendance.create!(user: benoit, concert: bruce_concert)
-puts "done!"
-
-puts "creating seed Sebastien attendance"
-sebastien = User.find_by(username: "Sebastien")
-Attendance.create!(user: sebastien, concert: bruce_concert)
-puts "done!"
-
-puts "creating seed Fred attendance"
-fred = User.find_by(username: "Fred")
-Attendance.create!(user: fred, concert: bruce_concert)
-puts "done!"
-
-puts "creating seed Stephane attendance"
-stephane = User.find_by(username: "Stephane")
-Attendance.create!(user: stephane, concert: bruce_concert)
-puts "done!"
-
-puts "creating seed Ines attendance"
-ines = User.find_by(username: "Ines")
-Attendance.create!(user: ines, concert: bruce_concert)
-puts "done!"
-
-puts "creating seed Jean-Claude attendance"
-jean-Claude = User.find_by(username: "Jean-Claude")
-Attendance.create!(user: jean-Claude, concert: bruce_concert)
-puts "done!"
-
-puts "creating seed Clarence attendance"
-clarence = User.find_by(username: "Clarence")
-Attendance.create!(user: clarence, concert: bruce_concert)
-puts "done!"
-
-puts "creating seed Flo attendance"
-flo = User.find_by(username: "Flo")
-Attendance.create!(user: flo, concert: bruce_concert)
-puts "done!"
+15.times do
+  user = User.where.not(username: "Cat").sample
+  puts "creating seed #{user.username} attendance"
+  Attendance.create!(user: user, concert: bruce_concert)
+  puts "done!"
+end
